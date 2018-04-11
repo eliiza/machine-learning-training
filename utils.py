1bf28e1934b575ed4279e5f0c51f99ca51d1838d
rayh <rayh@local>
2018-04-11 06:37:08 +0000

    Extract encode_features

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+
 def evaluate_model(model_fn):
     '''
     Consumes a function model_fn
@@ -13,3 +14,48 @@ def evaluate_model(model_fn):
     mae = np.mean(np.abs(predicted_saleprice-actual_values))
     print("The model is inaccurate by $%.2f on average." % mae)
     return mae
+
+
+def encode_features(data):
+    features = data.copy()
+    
+    # Encode Central Air where Y is 1, and N is 0
+    features['CentralAir'] = features['CentralAir'] == 'Y'
+
+    # Encode basement condition using one-hot-encoding
+    bsmt_cond_map = {'Ex':5,'Gd':4,'TA':3,'Fa':2,'Po':1}
+    features['BsmtQuality'] = features['BsmtCond'].map(bsmt_cond_map).fillna(0) # Some houses have no basement
+
+    # Encode electrical
+    features = pd.concat([features, encode_electrical(features['Electrical'])], axis=1)
+    
+    # Combine bed and bath    
+    features['BedBath'] = features['FullBath'] * features['BedroomAbvGr']
+    
+    # Scale numeric values
+    scaler = MinMaxScaler()
+    scaled_columns = [
+        'FullBath',
+        'BedroomAbvGr', 
+        'BedBath',
+        'GrLivArea',
+        'CentralAir', 
+        'FuseA',
+        'FuseF',
+        'FuseP', 
+        'Mix',
+        'SBrkr', 
+        'BsmtQuality'
+    ]
+    scaled_features = features[scaled_columns]
+    return pd.DataFrame(scaler.fit_transform(scaled_features), columns = scaled_columns)
+
+def encode_label(data):
+    labels = data.copy()['SalePrice']
+    scaler = MinMaxScaler()
+    scaler.fit(labels)    
+    labels = pd.DataFrame(scaler.transform(labels), columns = ['SalePrice'])
+    return (labels['SalePrice'], scaler)
+
+def decode_label(data, scaler):
+    return scaler.inverse_transform(data)
